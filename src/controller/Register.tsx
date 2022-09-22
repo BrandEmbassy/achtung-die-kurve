61c1dce55c520a48e970b2c47c6b6979d9c786e5
Jindřich Samec <jindrich.samec@xjs.cz>
2022-09-22 12:00:47 +0200

    link between Register & Play

--- src/controller/Register.tsx
+++ src/controller/Register.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 export const Register: React.FC<{}> = () => {
+
+  const {gameId, userId} = useParams()
   return (
     <div>
       Register
+      <Link to={`/controller/${gameId}/play/${userId}`}>Start</Link>
     </div>
   )
 }
