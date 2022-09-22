6603d0f59894e3631fcf66ecfb9af3352de2adb4
Jan Prokop <jan.prokop@niceincontact.com>
2022-09-22 11:17:58 +0200

    Router init - redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import { BrowserRouter, redirect, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, redirect, Route, Routes } from "react-router-dom";
 import { Game } from './game'
 import { Controller } from './controller'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        Hello
-        <Route path="/game" element={<Game />} />
-        
+        <Route path="/game" element={<Game />} id="game" />
         <Route path="/controller" element={<Controller />} />
-        {/* <Route path='*' action={() => redirect("/game")} /> */}
+        <Route path='*' element={ <Navigate replace to="/game" />} />
       </Routes>
     </BrowserRouter>
   );
