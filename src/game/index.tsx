09faa7b0be5cfec835b28015954325511e561dc7
Snehov <snehov@gmail.com>
2022-09-22 12:11:27 +0200

    lobby component add

--- src/game/index.tsx
+++ src/game/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import NewGameRedirect from './NewGameRedirect'
-import Lobby from './lobby'
+import { Lobby } from './lobby'
 import Play from './play'
-
 
 export const Game = (): JSX.Element => {
   return (
     <Routes>
       <Route path="/:gameId" element={<Lobby />} />
       <Route path="/:gameId/play" element={<Play />} />
-      <Route path='/' element={<NewGameRedirect />} />
+      <Route path="/" element={<NewGameRedirect />} />
     </Routes>
   )
 }
