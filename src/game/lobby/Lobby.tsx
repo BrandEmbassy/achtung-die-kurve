1fed41c4345cf9099b6eba9c0885c8fe2be116fe
Snehov <snehov@gmail.com>
2022-09-22 13:26:21 +0200

    add play url at lobby

--- src/game/lobby/Lobby.tsx
+++ src/game/lobby/Lobby.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {
+  Link,
   useLocation,
   useNavigate,
   useNavigation,
@@ -16,17 +17,19 @@ export const Lobby = () => {
   const { gameId, ...rest } = useParams()
 
   const gameUrl = `${window.location.protocol}//${window.location.host}/controller/${gameId}`
+  const playUrl = `${window.location.protocol}//${window.location.host}/game/${gameId}/play`
 
   const players = usePlayers(gameId)
   return (
     <>
       <h1>Achtung ty kurvo!</h1>
       <div>the game</div>
-      Open game at this URL: {gameUrl}
+      Open game at this URL: <a href={gameUrl} >{gameUrl}</a>
       <QrGenerator url={gameUrl} />
       <PlayersList players={players} />
       <div>Waiting for other players</div>
       <div>when all players ready, pres Play on your controller</div>
+      <div>or hit <a href={playUrl}>PLAY!</a></div>
     </>
   )
 }
