04afc1e1a454b1907356edb02c7549005a5e7fc3
Jindřich Samec <jindrich.samec@xjs.cz>
2022-09-22 11:52:30 +0200

    controller routes

--- src/controller/index.tsx
+++ src/controller/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Register } from './Register'
+import { Play } from './Play'
+
 interface ControllerProps {
-  
+
 }
 
 export const Controller = (props: ControllerProps): JSX.Element => {
   return (
-    <div>Controller</div>
+    <Routes>
+      <Route path="/:gameId/register/:userId" element={<Register />} />
+      <Route path='/:gameId/play/:userId' element={<Play />} />
+    </Routes>
   )
 }
