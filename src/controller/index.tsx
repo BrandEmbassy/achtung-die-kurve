55304af7e7d1f8ce54e9486ce1885986ba3ba73e
Jindřich Samec <jindrich.samec@xjs.cz>
2022-09-22 12:04:52 +0200

    create new userId

--- src/controller/index.tsx
+++ src/controller/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate, useParams } from 'react-router-dom'
 import { Register } from './Register'
 import { Play } from './Play'
+import { v4 as uuidv4 } from 'uuid';
 
 interface ControllerProps {
 
@@ -10,8 +11,20 @@ interface ControllerProps {
 export const Controller = (props: ControllerProps): JSX.Element => {
   return (
     <Routes>
+      <Route path="/:gameId" element={<NewUserRedirect />} />
       <Route path="/:gameId/register/:userId" element={<Register />} />
       <Route path='/:gameId/play/:userId' element={<Play />} />
     </Routes>
+  )
+}
+
+
+export const NewUserRedirect = (props) => {
+
+  const newUserId = uuidv4()
+  const {gameId} = useParams()
+
+  return (
+    <Navigate to={`/controller/${gameId}/register/${newUserId}`} />
   )
 }
